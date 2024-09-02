3dbfadc573dba22dbef7060b1fa1aa53029054bf
myllez2110 <bapniel21@gmail.com>
2024-09-02 14:45:59 -0300

    Exercício 5 e alteração na main.

--- src/main/java/com/lista01/app/AppApplication.java
+++ src/main/java/com/lista01/app/AppApplication.java
@@ -8,10 +8,13 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
-		Exercicio01.resultado();
-		Exercicio02.resultado();
-		Exercicio03.resultado();
-		Exercicio04.resultado();
+		//Retirar comentário para testar o devido exercício, já que não foi pedido menu.
+		//Exercicio01.resultado();
+		//Exercicio02.resultado();
+		//Exercicio03.resultado();
+		//Exercicio04.resultado();
+		//Exercicio05.resultado();
+		
 	}
 
 }
