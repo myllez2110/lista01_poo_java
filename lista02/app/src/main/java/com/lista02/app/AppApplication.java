9571a6ed1648252c77219be921d34dffbbe39cb5
myllez2110 <bapniel21@gmail.com>
2024-09-03 15:02:25 -0300

    Nova mudança na raiz do git para comportar as listas 01,02 e fufuturamente a 03 e 04, pedido do professor de ser em um repositório

--- lista02/app/src/main/java/com/lista02/app/AppApplication.java
+++ lista02/app/src/main/java/com/lista02/app/AppApplication.java
@@ -1,4 +1,4 @@
-package com.lista01.app;
+package com.lista02.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,6 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
-		//Retirar comentário para testar o devido exercício, já que não foi pedido menu.
 		//Exercicio01.resultado();
 		//Exercicio02.resultado();
 		//Exercicio03.resultado();
@@ -16,6 +15,7 @@ public class AppApplication {
 		//Exercicio05.resultado();
 		//Exercicio06.resultado();
 		//Exercicio07.resultado();
-		Exercicio08.resultado();
+		//Exercicio08.resultado();
 	}
+
 }
